530b4d6f131db6ff0d4d45e20ab80c11faa9d977
Irni Dwi Adryani <irnidwi16@gmail.com>
2024-08-23 16:15:50 +0700

    feat: create relation for models

---
+++ server/models/Association.js
@@ -0,0 +1,13 @@
+const Books = require("./BookModel");
+const Loans = require("./LoansModel");
+const Category = require("./CategoryModel");
+
+
+Books.hasMany(Loans, { foreignKey: "book_id", as: 'bookloans' });
+Loans.belongsTo(Books, { foreignKey: "book_id", as: 'book' });
+
+
+Books.belongsTo(Category, { foreignKey: "category_id", as: "category" });
+Category.hasMany(Books, { foreignKey: "category_id", as: "books" });
+
+module.exports = { Books, Loans, Category };
