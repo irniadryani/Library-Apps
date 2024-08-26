6ac1169fc310d8156c442afd99b5c017a5917a98
Irni Dwi Adryani <irnidwi16@gmail.com>
2024-08-26 07:37:01 +0700

    feat: add location for list

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { logoutUserFn } from "../../api/Auth/Auth";
 import useStore from "../../store/index";
 import { toast } from "react-toastify";
@@ -7,38 +7,37 @@ import { BiSolidHome } from "react-icons/bi";
 import { ImList2 } from "react-icons/im";
 import { GrTransaction } from "react-icons/gr";
 import { IoLogOut } from "react-icons/io5";
-import Logo from "../../assets/logo.png"
+import Logo from "../../assets/logo.png";
 
 const Sidebar = () => {
   const store = useStore();
   const navigate = useNavigate();
+  const location = useLocation();
   const handleLogout = async () => {
-
     const result = await Swal.fire({
-      title: 'Are you sure?',
-      text: 'You will be logged out of your account.',
-      icon: 'warning',
+      title: "Are you sure?",
+      text: "You will be logged out of your account.",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, log out',
-      cancelButtonText: 'Cancel'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, log out",
+      cancelButtonText: "Cancel",
     });
-  
-    
+
     if (result.isConfirmed) {
       try {
-        await logoutUserFn(); 
-        store.logout(); 
-        navigate('/login'); 
-        toast.success('You successfully logged out'); 
+        await logoutUserFn();
+        store.logout();
+        navigate("/login");
+        toast.success("You successfully logged out");
       } catch (error) {
-        console.error('Logout error:', error);
-        toast.error('Logout failed. Please try again.'); 
+        console.error("Logout error:", error);
+        toast.error("Logout failed. Please try again.");
       }
     }
   };
-  
+
   return (
     <div className="flex justify-start items-start">
       <div className="drawer !h-screen lg:drawer-open">
@@ -51,17 +50,37 @@ const Sidebar = () => {
             className="drawer-overlay"
           ></label>
           <ul className="menu bg-[#C9DABF] text-[#5F6F65] text-lg font-bold !h-screen w-80 p-4 overflow-y-auto mx-5 rounded-xl shadow-xl ">
-            <li className="mt-5">
-              <a href="/"><BiSolidHome />Dashboard</a>
+            <li 
+              className={`mt-5 ${
+                location.pathname === "/" ? "bg-[#9db391] rounded-lg text-white" : ""
+              }`}
+            >
+              <a href="/">
+                <BiSolidHome />
+                Dashboard
+              </a>
             </li>
-            <li className="mt-3">
-              <a href="/book"><ImList2/>List Books</a>
+            <li className={`mt-5 ${
+                location.pathname === "/book" ? "bg-[#9db391] rounded-lg text-white" : ""
+              }`}>
+              <a href="/book">
+                <ImList2 />
+                List Books
+              </a>
             </li>
-            <li  className="mt-3">
-              <a href="/transaction"><GrTransaction />Transaction</a>
+            <li className={`mt-5 ${
+                location.pathname === "/transaction" ? "bg-[#9db391] rounded-lg text-white" : ""
+              }`}>
+              <a href="/transaction">
+                <GrTransaction />
+                Transaction
+              </a>
             </li>
-            <li  className="mt-3 mb-5">
-              <a onClick={() => handleLogout()}><IoLogOut/>Logout</a>
+            <li className="mt-3 mb-5">
+              <a onClick={() => handleLogout()}>
+                <IoLogOut />
+                Logout
+              </a>
             </li>
           </ul>
         </div>
